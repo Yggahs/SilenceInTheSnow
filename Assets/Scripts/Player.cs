748e3a54b4208b96c326f10bccd46a2195e06a22
Yggahs <alessio.calore96@gmail.com>
2018-12-03 21:29:15 +0000

    Finish vaffanculo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : Photon.MonoBehaviour, IPunObservable
+public class Player : Photon.MonoBehaviour/*, IPunObservable*/
 
 {
 
@@ -199,60 +199,58 @@ public class Player : Photon.MonoBehaviour, IPunObservable
             photonView.RPC("ChangeColorTo", PhotonTargets.OthersBuffered, color);
     }
 
-    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
+    //void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //{
 
-        if (stream.isWriting)
-        {
-            switch (PhotonNetwork.player.ID)
-            {
-                case 4:
-                    stream.SendNext(GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text);
-                    break;
-                case 3:
-                    stream.SendNext(GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text);
-                    break;
-                case 2:
-                    stream.SendNext(GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text);
-                    break;
-                case 1:
-                    stream.SendNext(GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text);
-                    print("kek");
-                    break;
+    //    if (stream.isWriting)
+    //    {
+    //        switch (PhotonNetwork.player.ID)
+    //        {
+    //            case 4:
+    //                stream.SendNext(GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text);
+    //                break;
+    //            case 3:
+    //                stream.SendNext(GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text);
+    //                break;
+    //            case 2:
+    //                stream.SendNext(GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text);
+    //                break;
+    //            case 1:
+    //                stream.SendNext(GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text);
+    //                print("kek");
+    //                break;
 
-            }
-            
+    //        }
+    //    }
+    //    else
+    //    {
+    //        switch (PhotonNetwork.player.ID)
+    //        {
+    //            case 4:
+    //                GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                break;
+    //            case 3:
+    //                GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                break;
+    //            case 2:
+    //                GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                break;
+    //            case 1:
+    //                GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text = (string)stream.ReceiveNext();
+    //                print("kek");
+    //                break;
 
-        }
-        else
-        {
-            switch (PhotonNetwork.player.ID)
-            {
-                case 4:
-                    GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    break;
-                case 3:
-                    GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    break;
-                case 2:
-                    GameObject.FindGameObjectWithTag("nick1").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    break;
-                case 1:
-                    GameObject.FindGameObjectWithTag("nick2").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick3").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    GameObject.FindGameObjectWithTag("nick4").GetComponent<Text>().text = (string)stream.ReceiveNext();
-                    print("kek");
-                    break;
-
-            }
+    //        }
 
         }
         
-    }
-}
+//    }
+//}
